c840dc34f16f425a4a970e6e33903e360d1bba7e
Leonardo Santos <92073728+BrandonliSantos@users.noreply.github.com>
2022-11-30 10:49:52 -0300

    feat: UsuarioDAO + ajuste classe usuario

--- src/Models/Usuario/Usuario.class.php
+++ src/Models/Usuario/Usuario.class.php
@@ -4,6 +4,7 @@
         public function __construct(
             private int $codigo = 0,
             private string $nome = "",
+            private string $email = "",
             private string $senha = "",
             private $tipo_usuario,
             private bool $ativo = true,
@@ -15,6 +16,8 @@
         public function getCodigo(){ return $this->codigo; }
 
         public function getNome(){ return $this->nome; }
+
+        public function getEmail(){ return $this->email; }
 
         public function getSenha(){ return $this->senha; }
 
@@ -29,6 +32,8 @@
         public function setCodigo(int $codigo){ $this->codigo = $codigo;}
 
         public function setNome(string $nome){ $this->nome = $nome;}
+
+        public function setEmail(string $email){ $this->email = $email;}
 
         public function setSenha(string $senha){ $this->senha = $senha;}
 
